66762db09466dceac620ee28b1b6b3f994891ee8
sakshikarmukale-hub <sakshikarmukale@gmail.com>
2026-06-15 18:00:05 +0530

    Create Home.jsx & Routing

--- Tastel Fine Food/src/App.jsx
+++ Tastel Fine Food/src/App.jsx
@@ -1,14 +1,17 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Routing from "./routes/Routing";
 import Header from "./components/Header";
-import './App.css'
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Routing />
       <Footer />
-    </>
+   </>
   );
 }
 
-export default App
+export default App;
