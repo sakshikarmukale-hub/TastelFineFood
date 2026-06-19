29812c8ad3d0d5dfdc101d2bfefed01050129271
sakshikarmukale-hub <sakshikarmukale@gmail.com>
2026-06-19 16:46:55 +0530

    home.jsx

--- Tastel Fine Food/src/pages/Home.jsx
+++ Tastel Fine Food/src/pages/Home.jsx
@@ -2028,6 +2028,10 @@ const Home = () => {
                       background: "#D7AF64",
                       color: "#fff",
                     },
+                     ml: {
+      xs: 30,   // mobile
+      md: 20,   // tablet/desktop
+    },
                   }}
                 >
                   Send
